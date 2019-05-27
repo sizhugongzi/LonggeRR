643830a810a6a5f219865573ec005feb6b724f2e
sizhugongzi <228699456@qq.com>
2019-05-27 15:52:25 +0800

    Initial Commit

---
+++ LonggeRR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LonggeRR
+//
+//  Created by 甘延娇 on 2019/5/27.
+//  Copyright © 2019 tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
